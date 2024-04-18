95517187eee9979e062173c1b71da8c84c08e092
Angelo <angelo.knto@gmail.com>
2024-04-18 16:33:18 -0300

    implemented Product model

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -1,6 +1,18 @@
-﻿namespace APIMongoDB.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIMongoDB.Entities
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)] public int Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+
     }
 }
