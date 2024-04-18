9bd7767704d6be63bb14260ba2b99f3c7d74625a
Angelo <angelo.knto@gmail.com>
2024-04-18 16:29:01 -0300

    implemented ProductContext

--- Data/ProductContext.cs
+++ Data/ProductContext.cs
@@ -1,6 +1,23 @@
-﻿namespace APIMongoDB.Data
+﻿using APIMongoDB.Entities;
+using MongoDB.Driver;
+
+namespace APIMongoDB.Data
 {
-    public class ProductContext
+    public class ProductContext : IProductContext
     {
+        public IMongoCollection<Product> Products { get; set; }
+        public ProductContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>
+                ("DatabaseSettings:ConnectionString"));
+
+            var database = client.GetDatabase(configuration.GetValue<string>
+                ("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>
+                ("DatabaseSettings:CollectionName"));
+
+            //ProductContextSeed.SeedData(Products);
+        }
     }
 }
