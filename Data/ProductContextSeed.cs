50fd4a079f3b8eed5a6b1e93e5050c8f4fe8b9b0
Angelo <angelo.knto@gmail.com>
2024-04-19 16:30:31 -0300

    implemented SeedData

--- Data/ProductContextSeed.cs
+++ Data/ProductContextSeed.cs
@@ -1,6 +1,28 @@
-﻿namespace APIMongoDB.Data
+﻿using APIMongoDB.Entities;
+using MongoDB.Driver;
+
+namespace APIMongoDB.Data
 {
     public class ProductContextSeed
     {
+        public static void SeedData (IMongoCollection<ProductContext> collection)
+        {
+            bool prodList = collection.Find(p => true).Any();
+            if (!prodList)
+            {
+                collection.InsertManyAsync(GetProducts());
+            }
+
+            private static IEnumerable<Product> GetProducts()
+            {
+                return new List<Product>()
+                {
+                    new Product() { Id = 1, Name = "", Category = "", Description = "", Image = "", Price = 1.5M },
+                    new Product() { Id = 1, Name = "", Category = "", Description = "", Image = "", Price = 1.5M },
+
+                };
+                }
+            }
+        }
     }
 }
