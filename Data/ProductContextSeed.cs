7e6bc7ef411dd29ed821ef8a123f85babe058ac1
Angelo <angelo.knto@gmail.com>
2024-04-19 17:05:04 -0300

    corrected prodSeed

--- Data/ProductContextSeed.cs
+++ Data/ProductContextSeed.cs
@@ -5,24 +5,23 @@ namespace APIMongoDB.Data
 {
     public class ProductContextSeed
     {
-        public static void SeedData (IMongoCollection<ProductContext> collection)
+        public static void SeedData (IMongoCollection<Product> collection)
         {
             bool prodList = collection.Find(p => true).Any();
             if (!prodList)
             {
                 collection.InsertManyAsync(GetProducts());
             }
+        }
 
-            private static IEnumerable<Product> GetProducts()
-            {
-                return new List<Product>()
+        private static IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>()
                 {
                     new Product() { Id = 1, Name = "", Category = "", Description = "", Image = "", Price = 1.5M },
                     new Product() { Id = 1, Name = "", Category = "", Description = "", Image = "", Price = 1.5M },
 
                 };
-                }
-            }
         }
     }
 }
