a95d57aed019b3dc59962b726edf7fe1a1d36cdf
Angelo <angelo.knto@gmail.com>
2024-04-20 12:13:16 -0300

    implemented ProdController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APIMongoDB.Entities;
+using APIMongoDB.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace APIMongoDB.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductRepository _repository;
+        public ProductController(IProductRepository repository)
         {
-            return View();
+            _repository = repository ??
+             throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProductById")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await _repository.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [Route("[action]/{category}", Name = "GetProductByCategory")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+        {
+            var products = await _repository.GetProductByCategory(category);
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+        {
+            await _repository.CreateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+        {
+            return Ok(await _repository.UpdateProduct(product));
+        }
+
+        [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+        public async Task<IActionResult> DeleteProductById(int id)
+        {
+            return Ok(await _repository.DeleteProduct(id));
         }
     }
 }
