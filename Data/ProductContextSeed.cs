d5de29a273c4e790cba35336a14bf753260e1079
Angelo <angelo.knto@gmail.com>
2024-04-20 14:00:55 -0300

    changes in ProdContextSeed items

--- Data/ProductContextSeed.cs
+++ Data/ProductContextSeed.cs
@@ -18,8 +18,33 @@ namespace APIMongoDB.Data
         {
             return new List<Product>()
                 {
-                    new Product() { Id = "1", Name = "teste", Category = "teste", Description = "teste", Image = "teste", Price = 1.5M },
-                    new Product() { Id = "2", Name = "teste", Category = "teste", Description = "teste", Image = "teste", Price = 1.5M }
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "Caderno Espiral Pequeno",
+                    Description = "Caderno espiral com 100 folhas pequeno capa dura",
+                    Image = "caderno.png",
+                    Price = 7.65M,
+                    Category = "MaterialEscolar"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f6",
+                    Name = "Borracha branca pequena",
+                    Description = "Borracha branca pequena para lápis",
+                    Image = "borracha.png",
+                    Price = 4.55M,
+                    Category = "MaterialEscolar"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a3990b47f7",
+                    Name = "Estojo de plástico pequeno",
+                    Description = "Estojo de plástico pequeno azul",
+                    Image = "estojo.png",
+                    Price = 6.79M,
+                    Category = "MaterialEscolar"
+                }
 
                 };
         }
